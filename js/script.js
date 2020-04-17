3eab257e8407d5b962b4ad9f0e340dca5893b791
Kasia Leśniewska <klesniewska29@gmail.com>
2020-04-17 18:47:43 +0200

    Add structure for nex task

--- js/script.js
+++ js/script.js
@@ -48,4 +48,31 @@
         link.addEventListener('click', titleClickHandler);
     }
 
+    // Część samodzielna looool
+
+    const optArticleSelector = '.post',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
+
+    function generateTitleLinks(){
+
+    /* remove contents of titleList */
+
+    /* for each article */
+
+        /* get the article id */
+
+        /* find the title element */
+
+        /* get the title from the title element */
+
+        /* create HTML of the link */
+
+        /* insert link into titleList */
+
+        
+    }
+
+    generateTitleLinks();
+
 }
