08cae44d36870633d158a6d01434c09421e0ac9b
Kasia Leśniewska <klesniewska29@gmail.com>
2020-04-17 16:33:50 +0200

    Correct remove .active from articles

--- js/script.js
+++ js/script.js
@@ -6,21 +6,22 @@
         console.log('Link was clicked!');
         console.log('event:', event);
     
-        /* remove class 'active' from all article links  */
+        /* [DONE] remove class 'active' from all article links  */
         const activeLinks = document.querySelectorAll('.titles a.active');
 
         for(let activeLink of activeLinks){
             activeLink.classList.remove('active');
         }
     
-        /* add class 'active' to the clicked link */
+        /* [IN PROGRESS] add class 'active' to the clicked link */
+        
+    
+        /* [DONE] remove class 'active' from all articles */
         const activeArticles = document.querySelectorAll('.posts article.active');
 
         for(let activeArticle of activeArticles){
             activeArticle.classList.remove('active');
         }
-    
-        /* remove class 'active' from all articles */
     
         /* get 'href' attribute from the clicked link */
     
