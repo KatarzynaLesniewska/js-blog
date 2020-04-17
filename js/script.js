821ef78ee15b52604cd193d7a5a68368ca2f3cc9
Kasia Leśniewska <klesniewska29@gmail.com>
2020-04-17 17:58:09 +0200

    Find the correct article

--- js/script.js
+++ js/script.js
@@ -34,8 +34,10 @@
         const articleSelector = clickedElement.getAttribute('href');
         console.log('clickedElementsAttribute:', articleSelector);
     
-        /* find the correct article using the selector (value of 'href' attribute) */
-    
+        /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+        const targetArticle = document.querySelector(articleSelector);
+        console.log('targetArticle:', targetArticle);
+
         /* add class 'active' to the correct article */
     }
     
